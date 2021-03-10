fc2b4a41d557eb900f86373b997344bad90f8d06
matthewcotton <matthewneilcotton@gmail.com>
2021-03-10 11:35:51 +0000

    create basic file setup, add user schema

---
+++ src/index.js
@@ -0,0 +1,38 @@
+// Require express and router
+const express = require("express");
+const router = require("./router");
+const cors = require("cors");
+
+// Use dotenv package to parse enviroment variables
+const dotenv = require("dotenv");
+dotenv.config();
+
+// Use mongoose to connect to database
+const mongoose = require("mongoose");
+mongoose.connect(
+  `mongodb+srv://${process.env.MONGO_USERNAME}:${process.env.MONGO_PASSWORD}@cluster0.3e1h8.mongodb.net/mealplan?retryWrites=true&w=majority`
+);
+const db = mongoose.connection;
+
+const app = express();
+
+// Check connection to database
+db.on("error", (err) => {
+  console.log(
+    `Error occured while connecting to the database. Error code ${err.message}`
+  );
+});
+db.once("open", () => {
+  console.log(`Sucessfully connected to the social-site database.`);
+});
+
+// Use json parser, cors and router
+app.use(express.json());
+app.use(cors());
+app.use(router);
+
+const port = process.env.PORT || 3001;
+
+app.listen(port, () => {
+  console.log(`Social site api listening at http://localhost:${port}`);
+});
---
+++ src/router.js
@@ -0,0 +1,45 @@
+const express = require("express");
+const router = express.Router();
+
+const authController = require("./Controllers/authController");
+
+/* Authentication */
+router.post("/auth", authController.auth);
+
+/* Define CRUD operations for users */
+// Get user data
+
+// Create a new user
+
+// Update username
+
+// Update password
+
+// Delete user
+
+
+/* Define CRUD operations for recipes */
+// Get all recipes
+
+// Get single recipe
+
+// Create new recipe
+
+// Update exisitng recipe
+
+// Delete a recipe
+
+
+/* Define CRUD operations for mealplans */
+// Get all mealplans
+
+// Get single mealplan
+
+// Create new mealplan
+
+// Update exisitng mealplan
+
+// Delete a mealplan
+
+
+module.exports = router;
